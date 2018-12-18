f2c1dd96afb069511480c814af33cf1c14040c37
OMMorticia <38755362+OMMorticia@users.noreply.github.com>
2018-12-18 12:23:25 -0600

    Checkpoint Feedback
    Added Polish to the Checkpoints

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField]
+    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 500 ;
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 1.5f;
+    private bool isActivated;
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+
+        if (isActivated)
+            scale = activatedScale;
+
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationspeed = inactivatedRotationSpeed;
+        if (isActivated)
+            rotationspeed = activatedRotationSpeed;
+
+        transform.Rotate(Vector3.up * rotationspeed * Time.deltaTime);
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
